c5e4ca4c1b149c411f89f6243eca709d76f9535d
dimar1150 <158440383+Dimar1510@users.noreply.github.com>
2024-07-30 20:37:39 +0300

    images fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       backgroundImage: {
         banner:
-          "linear-gradient(rgba(0,0,0,0.2), rgba(0,0,0,0.2)), url('./src/assets/images/banner.jpeg')",
-        cloud: "url('./src/assets/images/cloud.png')",
-        mountain: "url('./src/assets/images/mountain.png')",
+          "linear-gradient(rgba(0,0,0,0.2), rgba(0,0,0,0.2)), url('../public/images/banner.jpeg')",
+        mountain: 'url("../public/images/mountain.png")',
         blogBg:
-          "linear-gradient(#090c1ad8, #090c1ad8), url('./src/assets/images/mountain5.png')",
-        teamBg: "url('./src/assets/images/teamBg.jpg')",
+          "linear-gradient(#090c1ad8, #090c1ad8), url('../public/images/blogBg.png')",
+        teamBg: 'url("../public/images/teamBg.jpg")',
       },
 
       gridTemplateColumns: {
