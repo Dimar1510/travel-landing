0f573d3c66a52b454abe824ea4b881446d957f44
dimar1150 <158440383+Dimar1510@users.noreply.github.com>
2024-07-30 11:47:02 +0300

    bg images config

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 const App = () => {
-  return <div className="text-red-400 h-svh bg-banner">Hey</div>;
+  return <div className="text-red-400 h-svh bg-teamBg">Hey</div>;
 };
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,11 @@ export default {
       backgroundImage: {
         banner:
           "linear-gradient(rgba(0,0,0,0.2), rgba(0,0,0,0.2)), url('./src/assets/images/banner.jpeg')",
-        cloud: "url('src/assets/images/cloud.png')",
-        mountain: "url('../public/images/mountain.png')",
+        cloud: "url('./src/assets/images/cloud.png')",
+        mountain: "url('./src/assets/images/mountain.png')",
         blogBg:
-          "linear-gradient(#090c1ad8, #090c1ad8), url('../public/images/mountain5.png')",
-        teamBg: "url('../public/images/teamBg.jpg')",
+          "linear-gradient(#090c1ad8, #090c1ad8), url('./src/assets/images/mountain5.png')",
+        teamBg: "url('./src/assets/images/teamBg.jpg')",
       },
 
       gridTemplateColumns: {
